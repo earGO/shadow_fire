8d21c99da3aa24cd8dc6c107a37826c1a09a9fb8
earGO <rh.edwin@gmail.com>
2019-12-01 15:34:47 -0600

    fixed media-sizing, moving to bluetooth interface and checkins

--- lib/widgets/main_screen_controls.dart
+++ lib/widgets/main_screen_controls.dart
@@ -18,16 +18,18 @@ class MainScreenControls extends StatelessWidget {
     final user = Provider.of<Users>(context).currentUser;
     final credits = Provider.of<Users>(context).currentCredits;
     final name = Provider.of<Users>(context).currentName;
+    final  screenWidth = MediaQuery.of(context).size.width;
+    final  screenHeight = MediaQuery.of(context).size.height;
     return Padding(
       padding: EdgeInsets.only(bottom: 84),
       child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
             Container(
-              margin: EdgeInsets.only(top: 40, left: 106),
-              padding: EdgeInsets.only(right: 16),
+              margin: EdgeInsets.only(top: screenHeight*0.05),
+              padding: EdgeInsets.symmetric(horizontal: screenWidth*.05),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: <Widget>[
                   if (user != null)
                     Text(
@@ -38,64 +40,67 @@ class MainScreenControls extends StatelessWidget {
                           fontWeight: FontWeight.w400),
                     ),
                   if (user != null)
-                    Text(
-                      '\$ ${credits.toString()}',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w400),
+                    Container(
+                      margin: EdgeInsets.only(left: screenWidth*0.05),
+                      child: Text(
+                        '\$ ${credits.toString()}',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w400),
+                      ),
                     )
+
                 ],
               ),
             ),
             Center(
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-
-                  Container(
-                    height: 280,
-                    padding: EdgeInsets.only(bottom: 40),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            MainScreenSmallIcon(
-                              iconCode: 59504,
-                              routeName: Shop.routeName,
-                            ),
-                            MainScreenSmallIcon(
-                              iconCode: 58146,
-                              routeName: InstalledImplants.routeName,
-                            ),
-                            MainScreenSmallIcon(
-                              iconCode: 57534,
-                              routeName: MessagesScreen.routeName,
-                            ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            MainScreenSmallIcon(
-                              iconCode: 59603,
-                              routeName: SocialScreen.routeName,
-                            ),
-                            MainScreenSmallIcon(
-                              iconCode: 59389,
-                              routeName: ProfileScreen.routeName,
-                            ),
-                          ],
-                        ),
-                      ],
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
+                      height:screenHeight*0.45,
+                      width: screenWidth*0.95,
+                      child:  Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              MainScreenSmallIcon(
+                                iconCode: 59504,
+                                routeName: Shop.routeName,
+                              ),
+                              MainScreenSmallIcon(
+                                iconCode: 58146,
+                                routeName: InstalledImplants.routeName,
+                              ),
+                              MainScreenSmallIcon(
+                                iconCode: 57534,
+                                routeName: MessagesScreen.routeName,
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              MainScreenSmallIcon(
+                                iconCode: 59603,
+                                routeName: SocialScreen.routeName,
+                              ),
+                              MainScreenSmallIcon(
+                                iconCode: 59389,
+                                routeName: ProfileScreen.routeName,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  PlayButton(),
-                ],
+                    PlayButton(),
+                  ],
+                ),
               ),
-            ),
           ]),
     );
   }
--- lib/widgets/main_screen_small_icon.dart
+++ lib/widgets/main_screen_small_icon.dart
@@ -9,12 +9,14 @@ class MainScreenSmallIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final  screenWidth = MediaQuery.of(context).size.width;
+    final  screenHeight = MediaQuery.of(context).size.height;
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: 12),
+      margin: EdgeInsets.symmetric(horizontal: screenWidth*0.021,vertical: screenHeight*0.017),
       child: Card(
         child: Container(
-          width: 96,
-          height: 96,
+          width: screenWidth*0.25,
+          height: screenWidth*0.25,
           child: InkWell(
             splashColor: Colors.blue.withAlpha(30),
             onTap: () {
@@ -22,7 +24,7 @@ class MainScreenSmallIcon extends StatelessWidget {
             },
             child: Icon(
               IconData(iconCode, fontFamily: 'MaterialIcons'),
-              size: 65,
+              size: screenWidth*0.185,
               color: Theme.of(context).primaryColor,
             ),
           ),
